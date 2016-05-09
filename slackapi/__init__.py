9b5053b3e9100f512fe5035df694b62809bfab70
Jacek Artymiak <jacek@artymiak.com>
2016-05-09 21:28:05 +0000

    Release 0.0.3

--- slackapi/__init__.py
+++ slackapi/__init__.py
@@ -9,8 +9,8 @@ what integration you choose to use.
 """
 
 __title__ = 'slackapi'
-__version__ = '0.0.2'
-__build__ = 0x000002
+__version__ = '0.0.3'
+__build__ = 0x000003
 __author__ = 'Jacek Artymiak'
 __license__ = 'GNU GENERAL PUBLIC LICENSE, Version 2, June 1991'
 __copyright__ = 'Copyright 2016 Jacek Artymiak'
