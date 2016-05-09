a690888046f924baf63a6117df5c9aae1b06e175
Jacek Artymiak <jacek@artymiak.com>
2016-05-09 13:22:16 +0000

    Release 0.0.1

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+setup(name='slackapi',
+      description='Slack API for humans.',
+      author='Jacek Artymiak',
+      author_email='jacek@artymiak.com',
+      url='https://github.com/badmetacoder/slackapi',
+      version='0.0.1',
+      py_modules=['slackapi.integrations'],
+      )
